29145c6e1368c9c4663f38c0894b661dd91e5921
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-05-25 12:16:35 -0600

    initial commit: sizing up

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let jonSnowAttack = 25;
+let jamieLannisterAttack = 35;
+
+if (jonSnowAttack > jamieLannisterAttack) {
+  console.log("Jon Snow has better attack than Jame Lannister");
+} else {
+  console.log("Jamie Lannister has better attack than Jon Snow");
+}
