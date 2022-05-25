5f858e33db8952ab38a34911acbb5decfeb3005a
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-05-25 12:30:08 -0600

    Jamie's first attack

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 "use strict";
 
+// Jon's Stats
 let jonSnowAttack = 25;
-let jamieLannisterAttack = 35;
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+// Jamie's Stats
+let jamieLannisterAttack = 107;
 
 if (jonSnowAttack > jamieLannisterAttack) {
   console.log("Jon Snow has better attack than Jame Lannister");
@@ -9,4 +14,13 @@ if (jonSnowAttack > jamieLannisterAttack) {
   console.log("Jon Snow and Jamie Lannister have the same attack");
 } else {
   console.log("Jamie Lannister has better attack than Jon Snow");
+}
+
+if (jonSnowHealth <= jamieLannisterAttack) {
+  console.log("Jon Snow has been slain");
+} else {
+  // jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
+  jonSnowHealth -= jamieLannisterAttack;
+
+  console.log(`Jon Snow's health is down to ${jonSnowHealth}`);
 }
