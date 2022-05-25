d83201efd03f51a6349a2d29d0f251bf646cabad
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-05-25 12:24:08 -0600

    fixed tie breaker issue

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let jamieLannisterAttack = 35;
 
 if (jonSnowAttack > jamieLannisterAttack) {
   console.log("Jon Snow has better attack than Jame Lannister");
+} else if (jonSnowAttack === jamieLannisterAttack) {
+  console.log("Jon Snow and Jamie Lannister have the same attack");
 } else {
   console.log("Jamie Lannister has better attack than Jon Snow");
 }
